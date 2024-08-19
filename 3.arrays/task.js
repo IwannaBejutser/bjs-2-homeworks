f315cc708a1990dca2df5008e348b66de0735607
Alexey Horsov <alexhorsov@mail.ru>
2024-08-19 09:43:34 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+	return (
+		arr1.length === arr2.length &&
+		arr1.every((element, index) => element === arr2[index])
+	);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users
+		.filter((user) => user.gender === gender)
+		.reduce((acc, user, _, arr) => acc + user.age / arr.length, 0);
+	return result;
 }
